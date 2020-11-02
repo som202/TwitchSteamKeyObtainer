8864602b11bbd127fd8faca84ed5a88d0a719890
Dan'ka <42348895+som2202@users.noreply.github.com>
2020-11-02 22:56:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import socket
+from pynput.keyboard import Controller, Key
+
+def enterKey(key):
+
+    keyboard = Controller()
+
+    keyboard.type(key)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+def findKey(s):
+
+    a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    t = 0
+    for i in range(len(s)):
+        try:
+            if (s[i] in a) and (s[i + 5] == '-') and (s[i + 11] == '-'):
+                t = i
+                s = s[t:]
+                s = s[0:17]
+                enterKey(s)
+                break
+        except:
+            continue
+
+server = 'irc.chat.twitch.tv'
+port = 6667
+username = input("Enter your twich username: ")
+token = input("Enter your oauth token: ")
+channel = '#' + input("Enter the twitch username whose chat you want to scan: ")
+
+sock = socket.socket()
+
+sock.connect((server, port))
+sock.send(f"PASS {token}\n".encode('utf-8'))
+sock.send(f"NICK {username}\n".encode('utf-8'))
+sock.send(f"JOIN {channel}\n".encode('utf-8'))
+
+while True:
+    res = sock.recv(2048).decode('utf-8')
+
+    if res.startswith('PING'):
+        sock.send("PONG\n".encode('utf-8'))
+
+    res = res.split(":")[-1]
+
+    findKey(res)
